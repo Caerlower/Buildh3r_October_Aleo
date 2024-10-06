5a8b444bc7ca5459c78e6a968caa5df7e950bad6
Manav Goyal <manucool123.rich@gmail.com>
2024-10-06 18:32:13 +0000

    updates

--- aleo_project/index.js
+++
@@ -1,8 +0,0 @@
-import { Account } from '@provablehq/sdk';
-
-const account = new Account();
-
-// Individual keys can then be accessed through the following methods
-const privateKey = account.privateKey();
-const viewKey = account.viewKey();
-const address = account.address();
